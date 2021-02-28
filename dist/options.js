1353cba8c157d647b675fde27f3397083bea7bc9
saifobeidat <saifobeidat@gmail.com>
2021-02-28 22:48:32 +0200

    Create options.js

---
+++ dist/options.js
@@ -0,0 +1,17 @@
+window.MathJax = {
+    jax: ["input/TeX", "output/HTML-CSS"],
+    'HTML-CSS': {
+      undefinedFamily: 'Amiri'
+    },
+    extensions: ["tex2jax.js","[arabic]/arabic.js"],
+    AuthorInit: function () {
+            MathJax.Ajax.config.path["arabic"] = "https://cdn.jsdelivr.net/gh/saifobeidat/arabic-mathjax/dist";
+        },
+    tex2jax: {
+      inlineMath: [
+        ['$', '$'],
+        ["\\(", "\\)"]
+      ],
+      processEscapes: true,
+    },
+  };
